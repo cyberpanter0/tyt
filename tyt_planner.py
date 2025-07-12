95ce48271158e4938ef7fcf38ef5d6eea1731074
Eyüp Han CAM <67067162+cyberpanter0@users.noreply.github.com>
2025-07-12 16:19:48 +0300

    Update tyt_planner.py

--- tyt_planner.py
+++ tyt_planner.py
@@ -43,9 +43,10 @@ KONU_VERILERI ={
     "Köklü Sayılar": { "zorluk": "Orta", "ortalama_soru": 2, "kategori": "Zor" },
     "Oran-Orantı": { "zorluk": "Orta", "ortalama_soru": 2, "kategori": "Zor" },
     "Denklem Çözme": { "zorluk": "Orta", "ortalama_soru": 2, "kategori": "Zor" },
-    "Kümeler": { "zorluk": "Orta", "ortalama_soru": 2, "kategori": "Zor" }
+    "Kümeler": { "zorluk": "Orta", "ortalama_soru": 2, "kategori": "Zor" },
     "Fonksiyonlar": {"zorluk": "Zor", "ortalama_soru": 1, "kategori": "Zor"}
-    },
+    
+  },
   "Geometri": {
     "Açılar & Üçgenler": { "zorluk": "Zor", "ortalama_soru": 4, "kategori": "Zor" },
     "Katı Cisimler": { "zorluk": "Zor", "ortalama_soru": 2, "kategori": "Zor" },
